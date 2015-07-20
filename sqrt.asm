982406b1ae2889dac1e05265c0760ba9046e083f
jfbraatz <jfbraatz@gmail.com>
2015-07-20 18:39:35 -0400

    Update sqrt.asm

--- sqrt.asm
+++ sqrt.asm
@@ -1,51 +1,59 @@
+;When sqrt is called, it calculates the integer square root of AC and writes it back to AC
+;The most this calculation will be off is 1, which will correspond to 1/16" in our final implementation.
 
-;The variable and loop names are garbage, and I don't think this code handles the input of zero properly, but this is the gist
-;of the integer square root algorithm. I don't even think SCASM would accept some of the names I have but I'll deal with that later
-
-
-    LOAD    N
+    LOAD    N	
     CALL    sqrt
 
-sqrt:
-    CALL    Numbits
-    SHIFT   &B1000000001
-    STORE   B
-    LOAD    numbits
-    AND     1
-    ADD     B
-    STORE   ceilnumbits/2 ;ceilnumbits/2 is a varaible name that literally holds the value ceil(numbits(N)/2)
+test:
+	OUT		LCD
+	JUMP	test
 
+sqrt:
+    CALL    Numbits  	 
+    SHIFT   -1
+    STORE   B
+    LOAD    bits 
+    AND     One
+    ADD     B
+    STORE   ceilnumbits/2
+    
     ;calculate 2^ceil(numbits(N)/2)
     LOAD    One
     STORE   x
     LOAD    ceilnumbits/2
-Loop1
+Loop1:
     ADDI    -1
     STORE   B
     LOAD    x
     SHIFT   1
     STORE   x
     LOAD    B
-    JPOS    Loop
-
+    JPOS    Loop1
 
 Loop3:
+	LOAD	N
+	STORE	num
+	LOAD	Zero
+	STORE	floorN/x
+	
 ;calculate floor(N/x)
-    LOAD    One
-    STORE   floorN/x
-    LOAD    N
-Loop2:
-    ADDI    -1
-    STORE   B
-    LOAD    floorN/x
-    SHIFT   &B1000000001
-    STORE   floorN/x        ;floorN/x is also a variable name that holds floor(N/x)
-    LOAD    B
-    JPOS    Loop2
+divide:
+	LOAD	floorN/x
+	ADDI	1
+	STORE 	floorN/x
+	LOAD	num
+	SUB		x
+	STORE	num
+	JPOS	divide
+	JZERO	done
+	LOAD	floorN/x
+	ADDI	-1
+done:
+    LOAD	floorN/x
 
-ADD     x
-SHIFT   &B1000000001
-STORE   y
+	ADD     x
+	SHIFT   -1
+	STORE   y	
     SUB     x
     ADDI    1
     JPOS    returnx
@@ -61,25 +69,25 @@ returnx:
 
 Numbits:
     JZERO   ItsZero
-    SHIFT   &B1000000001
+    SHIFT   -1
     STORE   A
-    LOAD    Counter
+    LOAD    bits
     ADDI    1
-    Store   Counter
+    Store   bits
     LOAD    A
     JUMP    Numbits
 ItsZero:
-    LOAD    Counter
+    LOAD    bits
     RETURN
-
-Counter:    DW  0
+    
 Zero:       DW  0
 One:        DW  1
-N:			DW	16
+N:			DW	25625 ;number to be square rooted
 A:			DW	0
 B:			DW	0
 x:			DW	0
 y:			DW	0
 ceilnumbits/2:	DW	0
 floorn/x:	DW	0
-
+bits:		DW	0
+LCD:      EQU &H06  ; primitive 4-digit LCD display
