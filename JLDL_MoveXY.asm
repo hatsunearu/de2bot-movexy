9c1578c7b97e703c5484644c7a05f0782196c226
jfbraatz <jfbraatz@gmail.com>
2015-07-21 18:14:54 -0400

    Update JLDL_MoveXY.asm

--- JLDL_MoveXY.asm
+++ JLDL_MoveXY.asm
@@ -117,6 +117,30 @@ Forever:
 	JUMP   Forever     ; Do this forever.
 DEAD:      DW &HDEAD   ; Example of a "local variable"
 
+;This is I think what our main code will look like
+
+MoveLoop:
+LOAD	address
+CALL	getRandTheta
+
+LOAD	destTheta
+CALL	Pivot
+
+LOAD	destR
+CALL	moveStraight
+
+LOAD	address
+ADDI	2
+STORE	address
+
+LOAD	moveCounter
+ADDI	-1
+STORE	moveCounter
+
+JPOS	MoveLoop
+
+moveCounter:	DW	8
+address:	DW	(address of 1st x coordinate in memory)
 ;***************************************************************
 ;* Input Coordinate, store to "Input" variable
 ;***************************************************************
@@ -709,16 +733,16 @@ sqrt:
     	LOAD    One
     	STORE   x
     	LOAD    ceilnumbits/2
-Loop1:
+sqrtLoop1:
     	ADDI    -1
     	STORE   B
     	LOAD    x
     	SHIFT   1
     	STORE   x
     	LOAD    B
-    	JPOS    Loop1
+    	JPOS    sqrtLoop1
 
-Loop3:
+sqrtLoop3:
 	LOAD	N
 	STORE	num
 	LOAD	Zero
@@ -746,7 +770,7 @@ done:
     	JPOS    returnx
     	LOAD    y
     	STORE   x
-    	JUMP    Loop3
+    	JUMP    sqrtLoop3
 returnx:
     	LOAD    x
     	RETURN
@@ -775,6 +799,68 @@ y:		DW	0
 ceilnumbits/2:	DW	0
 floorn/x:	DW	0
 bits:		DW	0
+
+;**********************************************************************
+; getRandTheta takes as its argument the address of the x coordinate in
+;memory, and it calculates the current x position minus the previous
+;and the same for y, and then calculates and stores R and Theta into
+;DestR and DestTheta.
+;************************************************************************
+getRandTheta:
+
+STORE	xn
+ADDI	-2
+STORE	xn-1
+ADDI	1
+STORE	yn-1
+ADDI	2
+STORE	yn
+
+;get displacements and load them into AtanX and AtanY
+LOAD	xn
+SUB		xn-1
+STORE	AtanX
+LOAD	yn
+SUB		yn-1
+STORE	AtanY
+CALL	Atan2
+STORE	DestTheta
+
+;theta has been stored, now calculate distance
+LOAD	AtanX
+STORE	m16sA
+STORE	m16sB
+CALL	Mult16s
+LOAD	mres16sL
+STORE	AtanX
+LOAD	AtanY
+STORE	m16sA
+STORE	m16sB
+CALL	Mult16s
+LOAD	mres16sL
+ADD		AtanX
+SHIFT	8
+CALL	sqrt
+;now we need to convert 1/16' into encoder ticks by multiplying by 18+5/16
+STORE	m16sA
+LOADI	18
+STORE	m16sB
+CALL	Mult16s
+STORE	DestR
+LOADI	5
+STORE	m16sB
+CALL	Mult16s
+SHIFT	-4
+ADD		DestR
+STORE	DestR
+
+RETURN
+
+xn:		DW	0
+yn:		DW	0
+xn-1:	DW	0
+yn-1:	DW	0
+
 ;***************************************************************
 ;* Subroutines
 ;***************************************************************
@@ -957,6 +1043,143 @@ StopLog:
 	CLI    &B0010      ; disable interrupt source 1 (timer)
 	RETURN
 	
+;******************************************************************************;
+; Atan2: 4-quadrant arctangent calculation                                     ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; Original code by Team AKKA, Spring 2015.                                     ;
+; Based on methods by Richard Lyons                                            ;
+; Code updated by Kevin Johnson to use software mult and div                   ;
+; No license or copyright applied.                                             ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; To use: store dX and dY in global variables AtanX and AtanY.                 ;
+; Call Atan2                                                                   ;
+; Result (angle [0,359]) is returned in AC                                     ;
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ;
+; Requires additional subroutines:                                             ;
+; - Mult16s: 16x16->32bit signed multiplication                                ;
+; - Div16s: 16/16->16R16 signed division                                       ;
+; - Abs: Absolute value                                                        ;
+; Requires additional constants:                                               ;
+; - One:     DW 1                                                              ;
+; - NegOne:  DW 0                                                              ;
+; - LowByte: DW &HFF                                                           ;
+;******************************************************************************;
+Atan2:
+	LOAD   AtanY
+	CALL   Abs          ; abs(y)
+	STORE  AtanT
+	LOAD   AtanX        ; abs(x)
+	CALL   Abs
+	SUB    AtanT        ; abs(x) - abs(y)
+	JNEG   A2_sw        ; if abs(y) > abs(x), switch arguments.
+	LOAD   AtanX        ; Octants 1, 4, 5, 8
+	JNEG   A2_R3
+	CALL   A2_calc      ; Octants 1, 8
+	JNEG   A2_R1n
+	RETURN              ; Return raw value if in octant 1
+A2_R1n: ; region 1 negative
+	ADDI   360          ; Add 360 if we are in octant 8
+	RETURN
+A2_R3: ; region 3
+	CALL   A2_calc      ; Octants 4, 5            
+	ADDI   180          ; theta' = theta + 180
+	RETURN
+A2_sw: ; switch arguments; octants 2, 3, 6, 7 
+	LOAD   AtanY        ; Swap input arguments
+	STORE  AtanT
+	LOAD   AtanX
+	STORE  AtanY
+	LOAD   AtanT
+	STORE  AtanX
+	JPOS   A2_R2        ; If Y positive, octants 2,3
+	CALL   A2_calc      ; else octants 6, 7
+	XOR    NegOne
+	ADDI   1            ; negate the angle
+	ADDI   270          ; theta' = 270 - theta
+	RETURN
+A2_R2: ; region 2
+	CALL   A2_calc      ; Octants 2, 3
+	XOR    NegOne
+	ADDI   1            ; negate the angle
+	ADDI   90           ; theta' = 90 - theta
+	RETURN
+A2_calc:
+	; calculates R/(1 + 0.28125*R^2)
+	LOAD   AtanY
+	STORE  d16sN        ; Y in numerator
+	LOAD   AtanX
+	STORE  d16sD        ; X in denominator
+	CALL   A2_div       ; divide
+	LOAD   dres16sQ     ; get the quotient (remainder ignored)
+	STORE  AtanRatio
+	STORE  m16sA
+	STORE  m16sB
+	CALL   A2_mult      ; X^2
+	STORE  m16sA
+	LOAD   A2c
+	STORE  m16sB
+	CALL   A2_mult
+	ADDI   256          ; 256/256+0.28125X^2
+	STORE  d16sD
+	LOAD   AtanRatio
+	STORE  d16sN        ; Ratio in numerator
+	CALL   A2_div       ; divide
+	LOAD   dres16sQ     ; get the quotient (remainder ignored)
+	STORE  m16sA        ; <= result in radians
+	LOAD   A2cd         ; degree conversion factor
+	STORE  m16sB
+	CALL   A2_mult      ; convert to degrees
+	STORE  AtanT
+	SHIFT  -7           ; check 7th bit
+	AND    One
+	JZERO  A2_rdwn      ; round down
+	LOAD   AtanT
+	SHIFT  -8
+	ADDI   1            ; round up
+	RETURN
+A2_rdwn:
+	LOAD   AtanT
+	SHIFT  -8           ; round down
+	RETURN
+A2_mult: ; multiply, and return bits 23..8 of result
+	CALL   Mult16s
+	LOAD   mres16sH
+	SHIFT  8            ; move high word of result up 8 bits
+	STORE  mres16sH
+	LOAD   mres16sL
+	SHIFT  -8           ; move low word of result down 8 bits
+	AND    LowByte
+	OR     mres16sH     ; combine high and low words of result
+	RETURN
+A2_div: ; 16-bit division scaled by 256, minimizing error
+	LOADI  9            ; loop 8 times (256 = 2^8)
+	STORE  AtanT
+A2_DL:
+	LOAD   AtanT
+	ADDI   -1
+	JPOS   A2_DN        ; not done; continue shifting
+	CALL   Div16s       ; do the standard division
+	RETURN
+A2_DN:
+	STORE  AtanT
+	LOAD   d16sN        ; start by trying to scale the numerator
+	SHIFT  1
+	XOR    d16sN        ; if the sign changed,
+	JNEG   A2_DD        ; switch to scaling the denominator
+	XOR    d16sN        ; get back shifted version
+	STORE  d16sN
+	JUMP   A2_DL
+A2_DD:
+	LOAD   d16sD
+	SHIFT  -1           ; have to scale denominator
+	STORE  d16sD
+	JUMP   A2_DL
+AtanX:      DW 0
+AtanY:      DW 0
+AtanRatio:  DW 0        ; =y/x
+AtanT:      DW 0        ; temporary value
+A2c:        DW 72       ; 72/256=0.28125, with 8 fractional bits
+A2cd:       DW 14668    ; = 180/pi with 8 fractional bits
 ;*******************************************************************************
 ; Mult16s:  16x16 -> 32-bit signed multiplication
 ; Based on Booth's algorithm.
